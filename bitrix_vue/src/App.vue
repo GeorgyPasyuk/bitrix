f4d1190483f84eb24d92e9c4cdef41fa88faace6
GeorgyPasyuk <115622303+GeorgyPasyuk@users.noreply.github.com>
2023-02-21 16:17:04 +0300

    верстка меню

--- bitrix_vue/src/App.vue
+++ bitrix_vue/src/App.vue
@@ -2,10 +2,25 @@
   <div class="window">
     <div class="main_nav_bar"></div>
     <div class="msg_box">
-      <div class="msg_nav_bar"></div>
+      <div class="msg_nav_bar">
+        <div class="icons_items">
+          <img src="./images/icons/msg.svg" alt="#" class="msg_icons" />
+          <img src="./images/icons/lines.svg" alt="#" class="msg_icons" />
+          <img
+            src="./images/icons/notifications.svg"
+            alt="#"
+            class="msg_icons"
+          />
+          <img src="./images/icons/calls.svg" alt="#" class="msg_icons" />
+          <img src="./images/icons/settings.svg" alt="#" class="msg_icons" />
+          <img src="./images/icons/news.svg" alt="#" class="msg_icons" />
+        </div>
+      </div>
       <div class="chats">
         <div class="search">
-          <input class="input_search" type="text" />
+          <span class="lupa"></span>
+          <span class="cross"></span>
+          <input class="input_search" placeholder="Поиск" type="text" />
         </div>
         <div class="chats_container">
           <div class="chat_item">
@@ -17,6 +32,11 @@
               <div class="latest_message">{{ latest_message }}</div>
             </div>
           </div>
+        </div>
+      </div>
+      <div class="empty_dialogue">
+        <div class="empty_dialogue_text">
+          Начните диалог, выбрав контакт из списка слева.
         </div>
       </div>
     </div>
